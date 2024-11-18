f5b11a811381f1d29f8e049c47ae9fdbf70cfe77
Arturo Popoli <arturo.popoli@unibo.it>
2024-11-18 15:00:05 +0100

    FDM_2D_final

--- src/04_FDM_2D/FDM_2D_s2.m
+++ src/04_FDM_2D/FDM_2D_s2.m
@@ -1,4 +1,4 @@
-function [phi,X,Y] = FDM_2D_s1(geom,t)
+function [phi,X,Y] = FDM_2D_s2(geom,BCs,t)
 %UNTITLED Summary of this function goes here
 %   geom: structure with info on the geometry
 %   t: source term
@@ -36,9 +36,37 @@ for j = 1:ny
         % compute linear index
         k = (j-1)*nx + i;
 
-        if i==1||i==nx||j==1||j==ny % on the boundary
+        if and(i==1,j==1) % on S-W corner
             K(k,k) = 1;
-            rhs(k) = 0; % boundary condition
+            rhs(k) = 0.5*(BCs.S.val+BCs.W.val); % average
+        elseif(j==1) % S-edge
+            K(k,k) = 1;
+            rhs(k) = BCs.S.val;
+        elseif(i==1) % W-edge
+            K(k,k) = 1;
+            rhs(k) = BCs.W.val;
+        elseif and(i==nx,j==ny) % N-E corner
+            K(k,k) = kc; % central
+            K(k,k-1) = 2*kx; % left
+            K(k,k-nx) = 2*ky; % bottom
+
+            rhs(k) = t(x(i),y(j))...
+                - 2*BCs.E.val/dx - 2*BCs.N.val/dy; % evaluates t at considered node
+        elseif(i==nx)
+            K(k,k) = kc; % central
+            K(k,k-1) = 2*kx; % left
+            K(k,k+nx) = ky; % top
+            K(k,k-nx) = ky; % bottom
+
+            rhs(k) = t(x(i),y(j)) - 2*BCs.E.val/dx; % evaluates t at considered node
+        elseif (j==ny) % N-edge
+            % internal nodes
+            K(k,k) = kc; % central
+            K(k,k+1) = kx; % right
+            K(k,k-1) = kx; % left
+            K(k,k-nx) = 2*ky; % bottom
+
+            rhs(k) = t(x(i),y(j)) - 2*BCs.N.val/dy; % evaluates t at considered node
         else
             % internal nodes
             K(k,k) = kc; % central
