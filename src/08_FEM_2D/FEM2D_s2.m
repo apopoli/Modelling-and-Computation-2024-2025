ed84c316e4275a52d33445afe34054df036f7a58
Arturo Popoli <arturo.popoli@unibo.it>
2024-12-16 11:31:30 +0100

    preliminaries for 2D FEM

---
+++ src/08_FEM_2D/FEM2D_s2.m
@@ -0,0 +1,13 @@
+function [phi]=FEM2D_s2(msh,p,t,BC)
+
+% import mesh info
+
+% cycle to compute and assemble element matrix and right-hand-side element array
+
+
+% Boundary conditions
+% nodal_BC = set_BCs_on_nodes(msh, BC); % BC values on nodes
+
+% Solve linear system
+
+end
